62fe5e9fca75529cd1bb39003b55896ee80ec793
Tapasweni <tapaswenipathak@gmail.com>
2014-07-29 23:52:46 +0530

    Search From Terminal,A Python Script Code Commit

---
+++ Search.py
@@ -0,0 +1,11 @@
+import urllib
+import json as m_json
+Query = raw_input ('Query: ')
+Query = urllib.urlencode ({ 'q' : Query })
+response = urllib.urlopen ('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + Query).read ()
+json = m_json.loads (response)
+results = json ['responseData'] ['results']
+for result in results:
+    title = result['title']
+    url = result['url']   
+    print ( title + '; ' + url )
