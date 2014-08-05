4e34c5fef2360442c0fca33cd0665ca5b10da590
Tapasweni <tapaswenipathak@gmail.com>
2014-08-05 23:40:02 +0530

    Indent JSON File Code Commit

---
+++ PrettyPrintJSON.py
@@ -0,0 +1,21 @@
+import sys
+import simplejson as json
+
+#Read Input File
+inputFile = open (sys.argv[1])
+input = json.load (inputFile)
+inputFile.close ()
+
+#If No Output File Found, Print on Console
+if len (sys.argv) < 3:
+  print json.dumps(input, sort_keys = False, indent = 2)
+
+#Otherwise Store The Output
+else:
+  outputFile = open (sys.argv[2], "w")
+  json.dump (input, outputFile, sort_keys = False, indent = 2)
+  outputFile.close ()
+
+
+
+
