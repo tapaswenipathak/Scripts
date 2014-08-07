690b860b3bf2c33b2858081583b96dd39b375dc3
Tapasweni <tapaswenipathak@gmail.com>
2014-08-07 22:00:53 +0530

    Pretty Print HTML Code Commit

---
+++ PPHTML.py
@@ -0,0 +1,5 @@
+from lxml import etree, html
+
+input_html = raw_input ()
+document_root = html.fromstring (input_html)
+print (etree.tostring (document_root, encoding='unicode', pretty_print=True))
