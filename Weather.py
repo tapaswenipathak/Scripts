b349726227d292151e9d35f4575dff448fd39a17
Tapasweni <tapaswenipathak@gmail.com>
2014-08-27 07:49:48 +0530

    Auto locate

--- Weather.py
+++ Weather.py
@@ -1,8 +1,27 @@
 import urllib2
 import json
+from urllib2 import urlopen
 
-location = raw_input ("Enter your location : ")
-response = urllib2.urlopen ('http://api.openweathermap.org/data/2.5/weather?q=' + location)
+import re
+
+#Get Public IP
+def getPublicIP () :
+	data = str (urlopen ('http://checkip.dyndns.com/').read ())
+	return re.compile (r'Address: (\d+.\d+.\d+.\d+)').search (data).group (1)
+
+IP =  str (getPublicIP ())
+
+#Get Location
+url = 'http://ipinfo.io/' + IP + '/json' 
+response = urlopen (url)
 data = json.load (response)
-print str (data['main']['temp'] - 273) + "C" 
+city = data['city']
+
+
+#Get Weather
+response = urllib2.urlopen ('http://api.openweathermap.org/data/2.5/weather?q=' + city)
+data = json.load (response)
+print "Your City : " + city
+print "Current Weather : " + str (data['main']['temp'] - 273) + "C" 
+
 
